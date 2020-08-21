767d1aa4a65d6d52623e66d5df1a1242df4685b3
Your Username <you@example.com>
2020-08-21 14:02:42 -0600

    attributes display correctly and finished validations

--- lib/avatar/cli.rb
+++ lib/avatar/cli.rb
@@ -21,10 +21,10 @@ class CLI
         input = gets.strip
         check_exit?(input)
         get_characters(input)
-        character_display(input)
-        get_character_info(input)
-        # character_info_display(input)
-        API.get_characters(input)
+        character_display
+        choose_again
+        # get_character_info(input)
+        # API.get_characters(input)
         
     end
 
@@ -39,52 +39,67 @@ class CLI
             API.get_characters("Water+Tribe")
         else
             puts "Invalid selection, please try again:"
+            puts ""
             start
         end
     end
 
-    def character_display(input)
+    def character_display
         puts "Please choose a character by number:"
         Characters.all.each.with_index(1) do |character,index|
             puts ""
             puts "#{index}: #{character.name}"
         end
+        
+        get_character_info
         # binding.pry
     end
 
-    def get_character_info(input)
+    def get_character_info
         input = gets.strip
-        puts "Character Information:"
+        check_exit?(input)
+        input = input.to_i
+        if input > 0 && input <= Characters.all.length
+        puts ""
         character = Characters.all.find.with_index(1) do |character,index|
-            input.to_i == index
-            # puts "#{index}
+            input == index
             # binding.pry
         end
             if character
-                puts "#{character.name}: Allies: #{character.allies}: Enemies: #{character.enemies}: Photo URL: #{character.photoUrl}"
+                puts "Character Information:"
+                puts "Name: #{character.name}"
+                puts "Allies: #{character.allies.join}"
+                puts "Enemies: #{character.enemies.join}"
+                puts "Photo URL: #{character.photoUrl}"
             else
                 puts "Invalid selection, please try again:"
             end
+        else
+            check_exit?(input)
+
+            puts "Invalid selection, please try again:"
+            character_display
+        end
         # binding.pry
     end
 
+    def choose_again
+        puts ""
+        puts "Would you like to make another selection: yes/no"
+        input = gets.strip
+        check_exit?(input)
+        if input == "yes"
+            Characters.all.clear
+            start
+        else 
+            exit
+        end
 
-    # def character_info_display(input)
-    #     input = gets.strip.to_i
-    #     puts "Character Information:"
-    #     API.get_characters(input)
-    #     Characters.all.each.with_index(1) do |name, allies, enemies, photoUrl|
-    #     # puts "Allies: #{self.input}"
-    #     # puts "Enemies: #{self.input}"
-    #     # puts "Photo Url: #{self.input}"
-    #     puts "#{name}: #{allies}: #{enemies}: #{photoUrl}"
-    # # binding.pry
-    #     end
-    # end
-
+    end
 
     def check_exit?(input)
-        if input.downcase == "exit"
+        # binding.pry
+        if input == "exit"
             exit
         end
     end
